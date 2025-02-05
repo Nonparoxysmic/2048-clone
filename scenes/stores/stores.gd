fab7b03c02bb07cbc7bc0d4117cd64f58de797bc
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2025-02-04 21:19:27 -0600

    Add save file for high scores

--- scenes/stores/stores.gd
+++ scenes/stores/stores.gd
@@ -3,6 +3,9 @@ extends Node
 
 var game_mode: Common.GameMode
 var _item_textures: Array[Texture2D]
+var _high_score_filename: String = "user://high_scores.save"
+var _high_scores: Array[int]
+var _high_score_names: Array[String]
 
 func _ready() -> void:
 	_item_textures = [
@@ -24,6 +27,11 @@ func _ready() -> void:
 			var message: String = "Invalid texture in node " + name + "."
 			printerr(message)
 			OS.alert(message, "Error")
+	_high_scores = []
+	_high_scores.resize(10)
+	_high_score_names = []
+	_high_score_names.resize(10)
+	_load_high_scores()
 
 
 func get_item_texture(type: Common.ItemType) -> Texture2D:
@@ -52,3 +60,30 @@ func get_item_texture(type: Common.ItemType) -> Texture2D:
 			return _item_textures[11]
 		_:
 			return _item_textures[0]
+
+
+func _load_high_scores() -> void:
+	if not FileAccess.file_exists(_high_score_filename):
+		return
+	var save_file: FileAccess = FileAccess.open(_high_score_filename, FileAccess.READ)
+	var json_string: String = save_file.get_line()
+	var json: JSON = JSON.new()
+	var parse_error: Error = json.parse(json_string)
+	if parse_error:
+		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+		return
+	var scores: Array = json.data["scores"]
+	var names: Array = json.data["names"]
+	for i: int in range(10):
+		_high_scores[i] = int(str(scores[i]))
+		_high_score_names[i] = str(names[i])
+
+
+func _save_high_scores() -> void:
+	var save_file: FileAccess = FileAccess.open(_high_score_filename, FileAccess.WRITE)
+	var data: Dictionary = {
+		"names": _high_score_names,
+		"scores": _high_scores
+	}
+	var json_string: String = JSON.stringify(data)
+	save_file.store_line(json_string)
