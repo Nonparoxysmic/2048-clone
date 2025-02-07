2292a576df8e96eaaa89aa1f883d8ada9345d4a9
Nonparoxysmic <69494951+Nonparoxysmic@users.noreply.github.com>
2025-02-07 12:05:08 -0600

    Add reward sprites

--- scenes/autoloads/textures/textures.gd
+++ scenes/autoloads/textures/textures.gd
@@ -2,6 +2,7 @@ extends Node
 ## global autoload
 
 var _item_textures: Array[Texture2D]
+var _reward_textures: Array[Texture2D]
 
 
 func _ready() -> void:
@@ -21,7 +22,26 @@ func _ready() -> void:
 	]
 	for texture: Texture2D in _item_textures:
 		if not texture:
-			var message: String = "Invalid texture in node " + name + "."
+			var message: String = "Invalid item texture in node %s" % name
+			printerr(message)
+			OS.alert(message, "Error")
+	_reward_textures = [
+		load("res://assets/sprites/missing_item_texture.png") as Texture2D,
+		load("res://assets/sprites/placeholder/rewards/star1.png") as Texture2D,
+		load("res://assets/sprites/placeholder/rewards/star2.png") as Texture2D,
+		load("res://assets/sprites/placeholder/rewards/star3.png") as Texture2D,
+		load("res://assets/sprites/placeholder/rewards/star4.png") as Texture2D,
+		load("res://assets/sprites/placeholder/rewards/bolt1.png") as Texture2D,
+		load("res://assets/sprites/placeholder/rewards/bolt2.png") as Texture2D,
+		load("res://assets/sprites/placeholder/rewards/bolt3.png") as Texture2D,
+		load("res://assets/sprites/placeholder/rewards/bolt4.png") as Texture2D,
+		load("res://assets/sprites/missing_item_texture.png") as Texture2D,
+		load("res://assets/sprites/missing_item_texture.png") as Texture2D,
+		load("res://assets/sprites/missing_item_texture.png") as Texture2D,
+	]
+	for texture: Texture2D in _reward_textures:
+		if not texture:
+			var message: String = "Invalid reward texture in node %s" % name
 			printerr(message)
 			OS.alert(message, "Error")
 
@@ -52,3 +72,31 @@ func get_item_texture(type: Common.ItemType) -> Texture2D:
 			return _item_textures[11]
 		_:
 			return _item_textures[0]
+
+
+func get_reward_texture(type: Common.ItemType) -> Texture2D:
+	match type:
+		Common.ItemType.BERRY_2:
+			return _reward_textures[1]
+		Common.ItemType.JUICE_4:
+			return _reward_textures[2]
+		Common.ItemType.COOKIE_8:
+			return _reward_textures[3]
+		Common.ItemType.TART_16:
+			return _reward_textures[4]
+		Common.ItemType.CAKE_PLAIN_32:
+			return _reward_textures[5]
+		Common.ItemType.CAKE_FROSTED_64:
+			return _reward_textures[6]
+		Common.ItemType.CAKE_DECORATED_128:
+			return _reward_textures[7]
+		Common.ItemType.PACKAGE_256:
+			return _reward_textures[8]
+		Common.ItemType.ICE_CREAM_CONE_512:
+			return _reward_textures[9]
+		Common.ItemType.SUNDAE_1024:
+			return _reward_textures[10]
+		Common.ItemType.BANANA_SPLIT_2048:
+			return _reward_textures[11]
+		_:
+			return _reward_textures[0]
--- scenes/item/item.gd
+++ scenes/item/item.gd
@@ -2,10 +2,11 @@ class_name Item
 extends Node2D
 
 @export var sprite: Sprite2D
+@export var reward_sprite: Sprite2D
 var _slide_time: float = 0.25
 
 func _ready() -> void:
-	if not sprite:
+	if (not sprite) or (not reward_sprite):
 		var message: String = "Missing sprite reference in node " + name + "."
 		printerr(message)
 		OS.alert(message, "Error")
@@ -13,6 +14,7 @@ func _ready() -> void:
 
 func set_sprite_texture(type: Common.ItemType) -> void:
 	sprite.texture = Textures.get_item_texture(type)
+	reward_sprite.texture = Textures.get_reward_texture(type)
 
 
 func move_item(x: int, y: int, fade: Common.Fade) -> void:
@@ -36,3 +38,13 @@ func set_hidden(do_hide: bool) -> void:
 		tween2.set_ease(Tween.EASE_OUT)
 		tween2.set_trans(Tween.TRANS_ELASTIC)
 		tween2.tween_property(sprite, "scale", Vector2.ONE, _slide_time * 4)
+
+
+func convert_to_reward() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property(sprite, "self_modulate", Color.TRANSPARENT, _slide_time)
+	tween.parallel().tween_property(reward_sprite, "self_modulate", Color.WHITE, _slide_time)
+	var tween2: Tween = create_tween()
+	tween2.set_ease(Tween.EASE_OUT)
+	tween2.set_trans(Tween.TRANS_ELASTIC)
+	tween2.tween_property(reward_sprite, "scale", Vector2.ONE, _slide_time * 4)
